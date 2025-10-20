a6e7046f7b0840d64afcb957af20546ff8340223
kyawhla-commit <bwarpay.bp8@gmail.com>
2025-10-20 09:23:09 +0630

    add list style with mui

--- hello-react/src/Item.jsx
+++ hello-react/src/Item.jsx
@@ -1,10 +1,33 @@
-import {ListItem, IconButton, ListItemText, List} from "@mui/material"
+import 
+{ 
+    ListItem,
+     IconButton,
+      ListItemText,
+       ListItemIcon 
+} from "@mui/material";
+
+import 
+{
+  Delete as DeleteIcon,
+  SquareOutlined as TodoIcon,
+  Check as DoneIcon,
+} from "@mui/icons-material";
+
 export default function Item({ item, del, toggle }) {
-  return <ListItem>
-    <button onClick={() => toggle(item.id)}>
-      {item.done ? "Undo" : "Check"}
-    </button>
-    {item.name}
-    <button onClick={() => {del(item.id)}}>DEL</button>
-  </ListItem>;
+  return <ListItem
+  secondaryAction={
+    <IconButton onClick={() => del(item.id)}>
+      <DeleteIcon color="error"/>
+    </IconButton>
+  }
+  >
+    <ListItemIcon>
+      <IconButton onClick={() => {toggle(item.id)}}>
+        {item.done ? <DoneIcon/> : <TodoIcon/> }
+      </IconButton>
+    </ListItemIcon>
+    <ListItemText>
+      {item.name}
+    </ListItemText>
+  </ListItem>
 }
